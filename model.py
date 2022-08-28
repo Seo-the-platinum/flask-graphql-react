2186c17b02219a15f94245c22b24284ce7230747
Eliseo Flores <eliseo_flores530@yahoo.com>
2022-08-27 17:58:31 -0700

    initial commit, installed dependencies and got graphql and python server up and running

---
+++ model.py
@@ -0,0 +1,27 @@
+from ariadne import QueryType, MutationType
+from uuid import uuid4
+
+mutation = MutationType()
+query = QueryType()
+orders = []
+
+@query.field("hello")
+def resolve_hello(_, info):
+    return "Hi there"
+
+class Coffee:
+   def __init__(self, size, name, coffee_type):
+       self.size = size
+       self.name = name
+       self.type = coffee_type
+       self.id = uuid4()
+
+@query.field("orders")
+def resolve_orders(_, info):
+    return orders
+
+@mutation.field("orderCoffee")
+def resolve_order_coffee(_, info, size, name, type):
+    newOrder = Coffee(size, name, type)
+    orders.append(newOrder)
+    return newOrder
